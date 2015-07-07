6c3ef18ef5950c22b8eebda9f8869aa10a5a91c2
Ho Jeannie <hc114@duke.edu>
2015-07-07 16:49:52 -0400

    adding using class instances in Ruby

---
+++ RubyExamples/ClassInstance.rb
@@ -0,0 +1,3 @@
+require 'C:\Users\Jeannie\workspace\RubyTest\RubyExamples\ClassInstance_Hello.rb'
+require 'C:\Users\Jeannie\workspace\RubyTest\RubyExamples\ClassInstance_World.rb'
+puts "Statement from Main"
---
+++ RubyExamples/ClassInstance_World.rb
@@ -0,0 +1,2 @@
+puts "Location"
+gets

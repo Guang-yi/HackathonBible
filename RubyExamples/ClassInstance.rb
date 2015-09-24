c8c91952f65f9bc2a22da47c1db6345b0dd95e8d
Michael Lee <gl83@duke.edu>
2015-09-24 18:05:24 +0100

    update

--- RubyExamples/ClassInstance.rb
+++
@@ -1,3 +0,0 @@
-require 'C:\Users\Jeannie\workspace\RubyTest\RubyExamples\ClassInstance_Hello.rb'
-require 'C:\Users\Jeannie\workspace\RubyTest\RubyExamples\ClassInstance_World.rb'
-puts "Statement from Main"
--- RubyExamples/ClassInstance_World.rb
+++
@@ -1,2 +0,0 @@
-puts "Location"
-gets
